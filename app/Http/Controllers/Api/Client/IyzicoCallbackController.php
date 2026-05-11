2ac3db2f7c88c6f72aaee3b46cc4c10b7629646a
Ahmet Şahin <ahmetsahin@Ahmet-MacBook-Pro-2.local>
2026-05-11 21:02:05 +0300

    callback  düzenlendi

--- app/Http/Controllers/Api/Client/IyzicoCallbackController.php
+++ app/Http/Controllers/Api/Client/IyzicoCallbackController.php
@@ -85,9 +85,9 @@ class IyzicoCallbackController extends Controller
             freelancer_notification($order->id, $order->freelancer_id, 'Order', __('You have a new order'));
 
             // Emails
-            try { Mail::to(get_static_option('site_global_email'))->send(new OrderMail($order->id, 'admin')); } catch (\Exception $e) {}
-            try { Mail::to($client->email)->send(new OrderMail($order->id, 'client')); } catch (\Exception $e) {}
-            try { Mail::to($freelancer->email)->send(new OrderMail($order->id, 'freelancer')); } catch (\Exception $e) {}
+            try { Mail::to(get_static_option('site_global_email'))->queue(new OrderMail($order->id, 'admin')); } catch (\Exception $e) {}
+            try { Mail::to($client->email)->queue(new OrderMail($order->id, 'client')); } catch (\Exception $e) {}
+            try { Mail::to($freelancer->email)->queue(new OrderMail($order->id, 'freelancer')); } catch (\Exception $e) {}
 
             (new OrderServiceApi())->send_order_chat_message($order);
 
--- app/Http/Controllers/Api/Freelancer/IyzicoCallbackController.php
+++ app/Http/Controllers/Api/Freelancer/IyzicoCallbackController.php
@@ -194,11 +194,11 @@ class IyzicoCallbackController extends Controller
     private function sendWalletEmail($name, $id, $email)
     {
         try {
-            Mail::to(get_static_option('site_global_email'))->send(new BasicMail([
+            Mail::to(get_static_option('site_global_email'))->queue(new BasicMail([
                 'subject' => __('Deposit Confirmation'),
                 'message' => __('A user just deposited to his wallet via Iyzico. ID: ') . $id
             ]));
-            Mail::to($email)->send(new BasicMail([
+            Mail::to($email)->queue(new BasicMail([
                 'subject' => __('Deposit Confirmation'),
                 'message' => __('Your wallet deposit was successful via Iyzico. ID: ') . $id
             ]));
@@ -208,11 +208,11 @@ class IyzicoCallbackController extends Controller
     private function sendPromotionEmail($name, $id, $email)
     {
         try {
-            Mail::to(get_static_option('site_global_email'))->send(new BasicMail([
+            Mail::to(get_static_option('site_global_email'))->queue(new BasicMail([
                 'subject' => __('Promotion package purchase email'),
                 'message' => __('A user just purchased a promotion package via Iyzico. ID: ') . $id
             ]));
-            Mail::to($email)->send(new BasicMail([
+            Mail::to($email)->queue(new BasicMail([
                 'subject' => __('Promotion package purchase email'),
                 'message' => __('Your promotion package purchase successfully completed via Iyzico. ID: ') . $id
             ]));
@@ -222,11 +222,11 @@ class IyzicoCallbackController extends Controller
     private function sendSubscriptionEmail($name, $id, $email)
     {
         try {
-            Mail::to(get_static_option('site_global_email'))->send(new BasicMail([
+            Mail::to(get_static_option('site_global_email'))->queue(new BasicMail([
                 'subject' => __('Subscription purchase email'),
                 'message' => __('A user just purchased a subscription via Iyzico. ID: ') . $id
             ]));
-            Mail::to($email)->send(new BasicMail([
+            Mail::to($email)->queue(new BasicMail([
                 'subject' => __('Subscription purchase email'),
                 'message' => __('Your subscription purchase successfully completed via Iyzico. ID: ') . $id
             ]));
