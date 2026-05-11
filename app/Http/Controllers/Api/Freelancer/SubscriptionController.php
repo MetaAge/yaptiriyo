1d2cc3d9285f4e95ebe9c666cf437c1bb08a0ce6
Ahmet Şahin <ahmetsahin@Ahmet-MacBook-Pro-2.local>
2026-05-12 00:12:14 +0300

    abonelik düzenlendi

--- app/Http/Controllers/Api/Freelancer/SubscriptionController.php
+++ app/Http/Controllers/Api/Freelancer/SubscriptionController.php
@@ -134,6 +134,7 @@ class SubscriptionController extends Controller
 
         $total_limit = UserSubscription::where('user_id',$user_id)
             ->where('payment_status','complete')
+            ->where('status', 1)
             ->whereDate('expire_date', '>', Carbon::now())
             ->sum('limit');
 
